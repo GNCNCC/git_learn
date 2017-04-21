06a8a2b48d98ad1bdd8bfbd65e02231bfd648723
GN <3036483456@qq.com>
2017-04-21 16:05:48 +0800

    笔试挂了～

--- test/test.c
+++ test/test.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
+#include<string.h>
 
-#define	TEST "fdfdf\n" "sdfsdfsdfds\n"
+#define	TEST(x) strlen(x),"江辉"\
+       	"傻逼\n"
 
 int main()
 {
-	printf(TEST);
+	char buf[40]={"aaaaa bbbbbb cccccccc"};
+	char a[20] = {0};
+	strncpy(a,buf+13,strlen(buf+13));
+	strcpy(buf+5,a);
+	printf("%s\n",buf);
+	printf("%d\n",strncmp(buf,"ab",2));
+	printf("%d %s",TEST("dddddd"));
 	return 0;
 
 }
