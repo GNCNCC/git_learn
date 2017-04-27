986a1ba9605c92b128a6efd22adc2f6b4f6acc3a
GN <3036483456@qq.com>
2017-04-27 17:31:39 +0800

    add dev Makefile 添加调试宏

--- dev/dev.c
+++ dev/dev.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 
+
 int main(void)
 {
 	printf("dev0\n");
+	printf("lslls\n");
+#ifdef debug
 	printf("dev1\n");
 	printf("dev2\n");
 	printf("dev3\n");
 	printf("dev4\n");
 	printf("dev5\n");
+#endif
 	
 	return 0;
 }
